8cd9582431192da1bf3a2a767201d9a1008c7ca0
AlWard505 <epicpigslav@gmail.com>
2024-10-06 02:54:51 +0100

    working on dialog

--- LD56 TinyCreatures/Assets/Scripts/CSS_ScriptController.cs
+++ LD56 TinyCreatures/Assets/Scripts/CSS_ScriptController.cs
@@ -5,37 +5,64 @@ using UnityEditor;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Linq;
 public class CSS_ScriptController : MonoBehaviour
 {
     bool _IsEvent = false;
-    public GameObject _DialogBox;
+    public GameObject _DialogBox, _customer;
     public bool testman = false;
+    public Transform _CustomerSpot, _Door;
     [System.Serializable]
     public struct Event
     {
         public bool isRandom;
         public string[] Dialog;
+        public string[] RightDialog;
+        public string[] WrongDialog;
+        public string[] CloseDialog;
+        [NonSerialized] public string[] currentDialog;
+
         public Sprite Character;
+        public float Speed;
         public PersonalityTraits ReleventTrait;
         public PersonalityTraits[] ClosestTraits;
-        [NonSerialized]
-        public int Progress;
+        [NonSerialized] public int Progress;
+        public string[] IsRight(PersonalityTraits dog)
+        {
+            if(dog == ReleventTrait)
+            {
+                currentDialog = RightDialog;
+            }
+            else if (ClosestTraits.Contains(dog))
+            {
+                currentDialog = CloseDialog;
+            }
+            else
+            {
+                currentDialog = WrongDialog;
+            }
+            return currentDialog;
+        }
     }
     public Event[] _Events;
     public enum PersonalityTraits
     {
-        Lethargic,Energetic, Mischievous, Clever, FireStarter, Hungry
+        Blank,Lethargic,Energetic, Mischievous, Clever, FireStarter, Hungry
     }
     Event CurrentEvent;
     TMP_Text _dialog;
-    public void CallEvent(Event Event)
+    public void CallEvent(Event Event, PersonalityTraits Dog = PersonalityTraits.Blank)
     {
-        _DialogBox.SetActive(true);
-        _IsEvent = true;
-        CurrentEvent = Event;
-        CurrentEvent.Progress = 0;
-        _dialog.text = CurrentEvent.Dialog[CurrentEvent.Progress];
-        CurrentEvent.Progress++;
+        if(Dog == PersonalityTraits.Blank)
+        {
+            _IsEvent = true;
+            CurrentEvent = Event;
+            CurrentEvent.Progress = 0;
+            StartCoroutine(EnterCustomer());
+            return;
+        }
+
+
     }
     public void NextLine()
     {
@@ -45,6 +72,8 @@ public class CSS_ScriptController : MonoBehaviour
             _IsEvent = false;
             return;
         }
+        _DialogBox.SetActive(true);
+
         _dialog.text = CurrentEvent.Dialog[CurrentEvent.Progress];
         CurrentEvent.Progress++;
     }
@@ -67,6 +96,25 @@ public class CSS_ScriptController : MonoBehaviour
             {
                 NextLine();
             }
+        }
+    }
+    public IEnumerator EnterCustomer()
+    {
+        _customer.GetComponent<Image>().sprite = CurrentEvent.Character;
+        while (_customer.transform.position.x > _CustomerSpot.position.x)
+        {
+            _customer.transform.Translate(Vector2.left *CurrentEvent.Speed *Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+        NextLine();
+
+    }    
+    public IEnumerator CustomerLeave()
+    {
+        while (_customer.transform.position.x < _Door.position.x)
+        {
+            _customer.transform.Translate(Vector2.right *CurrentEvent.Speed *Time.deltaTime);
+            yield return new WaitForEndOfFrame();
         }
     }
 }
